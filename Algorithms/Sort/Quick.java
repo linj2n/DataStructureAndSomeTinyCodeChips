6ffb907c48c8375252d22b7ddd99fd3f994b5aae
linj2n <linj2n@163.com>
2018-01-09 13:34:55 +0800

    modfied the hoarePartition andadd lomutoParttion algorithm for quick sort.

--- Algorithms/Sort/Quick.java
+++ Algorithms/Sort/Quick.java
@@ -1,35 +1,63 @@
 import java.util.*;
 
 public class Quick
-{	
-	private static void sort(int[] a,int lo, int hi)
-	{
-		if (hi <= lo)	return;
-		int j = partition(a, lo, hi);
-		sort(a, lo, j-1);
-		sort(a, j+1, hi);
-	}
-	private static int partition(int[] a, int lo, int hi)
-	{
-		int i = lo, j = hi + 1;
-		int v = a[lo];
-		while (true)
-		{
-			while (a[++i] < v) if (i == hi) break;
-			while (v < a[--j]) if (j == lo) break;
-			if (i >= j) break;
-			int temp = a[i]; a[i] = a[j]; a[j] = temp;
-		}
-		int temp = a[lo]; a[lo] = a[j]; a[j] = temp;
-		return j;
-	}
-	public static void main(String [] args)
-	{
-		int [] a = {13,19,9,5,12,8,7,4,11,2,6,21};
-		Quick.sort(a,0, 11);
-		for (int i: a)
-			System.out.print(i + " ");
-		System.out.println("");
+{   
+    // [lo..hi] 
+    private static void sort(int[] a,int lo, int hi)
+    {
+        if (hi <= lo)   return;
+        int j = hoarePartition(a, lo, hi);
+        sort(a, lo, j-1);
+        sort(a, j+1, hi);
+    }
+    // lo 作为pivot,对a原地划分重排
+    private static int hoarePartition(int[] a, int lo, int hi)
+    {
+        int pValue = a[lo];
+        int i = lo + 1, j = hi;
+        while (true)
+        {
+            while (i < hi && a[i] < pValue) 
+            {
+                i = i + 1; 
+            }
+            while (j > lo && a[j] > pValue)
+            {
+                j = j - 1;
+            }
+            if (i >= j) break;
+            else exchange(a,i,j);
+        }
+        exchange(a,lo,j);
+        return j;
+    }
+    // lo 作为pivot,对a原地划分重排
+    private static int lomutoPartition(int[] a, int lo, int hi)
+    {
+        int s = lo;
+        int pValue = a[lo];
+        for (int i = lo + 1; i <= hi; i++)
+        {
+            if (a[i] < pValue)
+            {
+                ++s; exchange(a, i, s);
+            }
+        }
+        exchange(a,lo,s);
+        return s;
+    }
+    private static void exchange(int[] a, int i, int j)
+    {
+        int temp = a[i]; a[i] = a[j]; a[j] = temp;
+    }
 
-	}
+    public static void main(String [] args)
+    {
+        int [] a = {13,19,9,5,12,8,7,4,11,2,6,21};
+        Quick.sort(a,0, 11);
+        for (int i: a)
+            System.out.print(i + " ");
+        System.out.println("");
+
+    }
 }
