080569709ae15b0f49e4e32ee02919869ce618df
linj2n <linj2n@163.com>
2018-01-13 09:41:08 +0800

    add quick 3-way sort algorithm.

---
+++ AlgorithmsInJava/quick3WaySort/Quick3WaySort.java
@@ -0,0 +1,28 @@
+package quick3WaySort;
+public class Quick3WaySort
+{
+    public static void exch(Comparable[] a, int i, int j)
+    {
+        if (i == j || i > a.length || j > a.length || i < 0 || j < 0) return;
+        Comparable temp = a[i]; a[i] = a[j]; a[j] = temp;
+    }
+    public static void sort(Comparable[] a)
+    {
+        sort(a,0,a.length - 1);
+    }
+    public static void sort(Comparable[] a,int lo, int hi)
+    {
+        if (hi <= lo) return;
+        int lt = lo, i = lo + 1, gt = hi;
+        Comparable pValue = a[lo];
+        while (i <= gt)
+        {   
+            int crs = a[i].compareTo(pValue);
+            if (crs < 0) exch(a,i,gt--);
+            else if (crs > 0) exch(a, i,lt++);
+            else i++;
+        }
+        sort(a,lo,lt - 1);
+        sort(a,gt+1,hi);
+    }
+}
---
+++ AlgorithmsInJava/quick3WaySort/Quick3WaySortTest.java
@@ -0,0 +1,61 @@
+package quick3WaySort;
+public class Quick3WaySortTest
+{
+	// some test-functions and print-functions
+	private static void testAndPrint(Integer a[], int lo, int hi)
+	{
+		System.out.println("---------------------");
+		System.out.print("before: ");
+		for (int e: a)
+			System.out.print(e + " ");
+		System.out.println(" ");
+		Quick3WaySort.sort(a,lo,hi);
+
+		System.out.print("after: ");
+		for (int e: a)
+			System.out.print(e + " ");
+		System.out.println(" ");
+	}
+	public static void test0()
+	{
+		Integer []t = {1,2,3,4,5,6,7};
+		testAndPrint(t,0,t.length - 1);
+	}
+	public static void test1()
+	{
+		Integer []t = {7,6,5,4,3,2,1};
+		testAndPrint(t,0,t.length - 1);
+	}
+	public static void test2()
+	{
+		Integer []t = {5,4,7,6,3,2,1,8};	
+		testAndPrint(t,0,t.length - 1);
+	}
+	public static void test3()
+	{
+		Integer []t = {7,7,7,7};	
+		testAndPrint(t,0,t.length - 1);
+	}
+	public static void test4()
+	{
+		Integer []t = {7,7};
+		testAndPrint(t,0,t.length - 1);
+	}
+	public static void test5()
+	{
+		Integer []t = {7};
+		testAndPrint(t,0,t.length - 1);
+	}
+
+
+	public static void main(String[] args) 
+	{
+		Quick3WaySortTest.test0();
+		Quick3WaySortTest.test1();
+		Quick3WaySortTest.test2();
+		Quick3WaySortTest.test3();
+		Quick3WaySortTest.test4();
+		Quick3WaySortTest.test5();
+
+	}
+}
