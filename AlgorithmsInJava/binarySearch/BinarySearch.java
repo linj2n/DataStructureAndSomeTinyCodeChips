1a931fff879cda724fc9cc645d121b6134a589ad
linj2n <linj2n@163.com>
2018-04-21 12:59:59 +0800

    add binary-Search file

---
+++ AlgorithmsInJava/binarySearch/BinarySearch.java
@@ -0,0 +1,21 @@
+// note:
+// 1. line 18, 检查 lo 是否越界
+// 2. lo, hi 的更新
+package AlgorithmsInJava.binarySearch;
+public class BinarySearch {
+    public static Comparable search(Comparable[] a, Comparable key) {
+        return search(a,0,a.length - 1,key);
+    }
+    public static Comparable search(Comparable[] a, int beg, int end, Comparable key) {
+        int lo = beg, hi = end - 1;
+        if (lo > hi || a == null) return null;
+        while (lo <= hi) {
+            int mid = lo + (hi - lo) / 2;
+            int cmp = a[mid].compareTo(key);
+            if (cmp < 0) lo = mid + 1;
+            else if (cmp > 0) hi = mid - 1;
+            else return a[mid];
+        }
+        return (lo >= beg || hi <= end - 1) ? a[lo] : null;
+    }
+}
---
+++ AlgorithmsInJava/binarySearch/BinarySearchTest.java
@@ -0,0 +1,43 @@
+package AlgorithmsInJava.binarySearch;
+public class BinarySearchTest {
+    public static boolean resultForTest(Comparable[] a, Comparable key) {
+        return key.equals(BinarySearch.search(a,key));
+    }
+    public static void test(Comparable[] a, Comparable key) {
+        System.out.print("test: { ");
+        for (int i = 0; i < a.length; i ++) {
+            System.out.print(a[i].toString());
+            if (i != a.length - 1) 
+                System.out.print(",");
+        }
+        System.out.print(" }");
+        System.out.println("key: " + key.toString());
+        System.out.println("result= " + resultForTest(a,key));
+        System.out.println("---------------------------");
+    }
+    public static void main(String[] args) {
+        test(new Integer[]{
+        },0);
+        test(new Integer[]{
+            0
+        },0);
+        test(new Integer[]{
+            0
+        },1);
+        test(new Integer[]{
+            0,1,2,3
+        },0);
+        test(new Integer[]{
+            0,1,2,3
+        },3);
+        test(new Integer[]{
+            0,1,2,3
+        },2);
+        test(new Integer[]{
+            0,1,1,3
+        },1);
+        test(new Integer[]{
+            0,1,2,3
+        },4);
+    }
+}
